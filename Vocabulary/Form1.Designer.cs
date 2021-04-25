0894ba606051b7f6deff936b5e18349dcebf9d95
theo <vasya-fotul@yandex.ru>
2021-04-25 20:33:08 +0200

    initial commit

---
+++ Vocabulary/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Vocabulary
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Очистити";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.Location = new System.Drawing.Point(185, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(418, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(185, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(418, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(185, 244);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(418, 23);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(185, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Почати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(327, 328);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 37);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Перевірити";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(833, 481);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Vocabulary/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace Vocabulary
+{
+    class Vok
+    {
+        private static string z;
+        public  string Z
+        {
+            get { return z; }
+
+
+            set { z = value; }
+
+            
+        }
+
+
+       
+       
+       
+    }
+
+    public partial class Form1 : Form
+
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string path = @"D:\Gittset\Vocabulary\Vocabulary\Vocabularywords.txt";
+            using (StreamReader Vocabulary = new StreamReader(path)) ;
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+
+                string linefromVocabulary;
+                string[] WordstoTranslate = new string[250];
+                string[] TranslatedWords = new string[250];
+
+                try
+                {
+
+
+
+                    for (int i = 0, k = 0; (linefromVocabulary = sr.ReadLine()) != null; i++, k++)
+                    {
+                        string[] wordForTranslate = linefromVocabulary.Split(new char[] { '-' });
+
+                        string word = wordForTranslate[0];
+                        string translatedword = wordForTranslate[1];
+
+
+                        WordstoTranslate[i] = word;
+                        TranslatedWords[k] = translatedword;
+
+                    }
+                }
+                catch
+
+                {
+                    string a = "Щось пішло не так";
+                    textBox1.Text = Convert.ToString(a);
+                }
+
+
+
+                Random random = new Random();
+                int randomWord = random.Next(250);
+                Vok wordtotranslate = new Vok();
+                wordtotranslate.Z = TranslatedWords[randomWord];
+                
+                textBox1.Clear();
+                textBox1.Text = Convert.ToString("Напишіть переклад слова " + WordstoTranslate[randomWord]);
+
+                textBox2.Clear();
+                textBox3.Clear();
+               
+               
+
+
+    }
+
+        }
+    
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          Vok wordtoTranslate = new Vok();
+
+            string WordtoTranslate = wordtoTranslate.Z;
+
+            string translatedWord = Convert.ToString(textBox2.Text);
+
+            if (WordtoTranslate == translatedWord)
+            {
+                textBox3.Text = "Все правильно!";
+
+
+            }
+            else
+            {
+
+                textBox3.Text = ("Праввильна відповідь : " + Convert.ToString(WordtoTranslate));
+
+            }
+
+
+
+        }
+    }
+}
